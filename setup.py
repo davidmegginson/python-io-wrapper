42c77e7382c1b86ff319c31065676f252adfff10
David Megginson <david.megginson@gmail.com>
2022-11-14 09:59:33 -0500

    Added MANIFEST.in to ensure license info goes to PyPi.

--- setup.py
+++ setup.py
@@ -6,10 +6,16 @@ import sys
 if sys.version_info < (3,):
     raise RuntimeError("Requires Python 3 or higher")
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='python-io-wrapper',
-    version='0.3',
+    version='0.3.1',
     description='IO wrapper to add missing methods from io.BaseIO',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
     author='David Megginson',
     author_email='contact@megginson.io',
     packages=['io_wrapper'],
